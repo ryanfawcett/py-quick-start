d339151a5c3d7f902f2e17a62bb9a06754f820f2
ryanfawcett <tangch3ng@mail.com>
2023-03-26 17:24:25 +0800

    feat: the comments

---
+++ 01-basic-syntax/02-comments.py
@@ -0,0 +1,9 @@
+"""
+This is a Multiple Line Comments
+
+The comments used for explanation, will NOT be executed.
+
+comments
+    - Single Line Comment: #
+    - Multiple Line Comment: A pair of three quotes and insert the comments in the quotes
+"""
