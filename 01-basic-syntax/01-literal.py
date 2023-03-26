7803b696030bdabeda22a850e50abfc3bccb86c3
ryanfawcett <tangch3ng@mail.com>
2023-03-26 16:55:35 +0800

    feat: what is the literal

---
+++ 01-basic-syntax/01-literal.py
@@ -0,0 +1,16 @@
+# what is the literal?
+# The values that are written in the code
+
+# such as...
+3.14
+100
+
+# or
+"Hello World!"
+
+# The literals can be printed as below
+print(3.14)
+
+print(100)
+
+print("Hello World!")
