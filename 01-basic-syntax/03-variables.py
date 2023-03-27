2df1065b45981de395f335c34987423687981447
ryanfawcett <tangch3ng@mail.com>
2023-03-27 20:15:57 +0800

    feat: how to define a variable

---
+++ 01-basic-syntax/03-variables.py
@@ -0,0 +1,16 @@
+# A variable is being used to store
+# the results of a calculation or represent a value
+
+# Define a variable for saying hello
+greet = "Hello World!"
+
+# then print the variable that we defined
+print(greet)
+
+# Also, a variable can store an integer, a float, etc.
+# Let's define a variable to store the wallet balance
+money = 20
+
+# A cup of Coca-Cola's price
+colaPrice = 3.5
+
