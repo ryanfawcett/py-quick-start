94992424d8b4a30bd030a99d3fe2894cd1f5c641
ryanfawcett <tangch3ng@mail.com>
2023-03-28 20:38:30 +0800

    feat: data type

---
+++ 01-basic-syntax/04-data-type.py
@@ -0,0 +1,15 @@
+# The data types in Python
+# We can get the data type for literals or variables using the function of type()
+greet_type = type("Hello World!")
+
+# 'str' means string
+print(greet_type)  # <class 'str'>
+
+money = 20
+print(type(money))  # <class 'int'>
+
+colaPrice = 3.5
+print(type(colaPrice))  # <class 'float'>
+
+# Note: we have to know that a variable doesn't have a data type
+# But the data it stores does.
