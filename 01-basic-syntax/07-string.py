0d40eb77966f03962d662f29bd8a023a2b62546a
ryanfawcett <tangch3ng@mail.com>
2023-04-08 11:40:35 +0800

    feat: how to use the string

---
+++ 01-basic-syntax/07-string.py
@@ -0,0 +1,19 @@
+# String
+
+# Single quotes
+greet = 'Hello World'
+
+# Quotes
+foo = "foo"
+
+# Triple quotes
+baz = """baz"""
+
+# How to define a quotes in String?
+name = 'ryan fawcett'
+_name = "'ryan fawcett'"
+
+# Of course, there's another way to define the quotes in a String
+# we can use the escapes '\'
+name_with_escapes = "\"ryan fawcett\""
+print(name_with_escapes)
