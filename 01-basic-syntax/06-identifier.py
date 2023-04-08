3c2cd1b0c0d71def8ed7cec5aaf66dbfbc1d0499
ryanfawcett <tangch3ng@mail.com>
2023-04-08 11:17:08 +0800

    feat: the identifier

---
+++ 01-basic-syntax/06-identifier.py
@@ -0,0 +1,26 @@
+# Identifier in Python
+
+"""
+    In the identifier naming, only the following are ALLOWED:
+        - English
+        - Chinese
+        - Number
+        - Underline (_)
+
+    Warning
+        - Chinese is NOT recommended
+        - Can't start with a number
+"""
+
+foo = "foo"
+Foo = "foo"
+
+# foo and Foo are 2 different variables
+
+# The Keywords can't be an identifier
+# Such as False | True | None | and | ...
+
+# 名字 = "张三"
+name = "Andrea"
+
+_name = "foo"
