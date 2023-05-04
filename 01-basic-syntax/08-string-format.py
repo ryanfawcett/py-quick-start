e43debacdd0f33d01a210739877205dc48f5c618
ryanfawcett <tangch3ng@mail.com>
2023-05-04 18:44:22 +0800

    feat: quick format for string

---
+++ 01-basic-syntax/08-string-format.py
@@ -0,0 +1,8 @@
+# String Format
+
+abs_path = r'C:/user/ldplayer/ldplayer.exe'
+
+print(f'the absolute path of LDPlayer is {abs_path}')
+
+# quick format
+# to be used for formatting where precision is not required
